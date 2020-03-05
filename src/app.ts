e97d659a4fd8035bf455fab5287aa553e8302f9c
Ricardo Jardim <ricardojardim@searchtecnologia.com.br>
2020-03-05 09:27:38 -0300

    Separated logger functionality to a service class for reusability.

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,23 @@
+import 'express-async-errors';
 import 'dotenv/config';
 import cors from 'cors';
 import express, {
   Request, Response, NextFunction,
 } from 'express';
 import morgan from 'morgan';
-import {
-  createLogger, format, transports, Logger,
-} from 'winston';
 import Youch from 'youch';
 
-import 'express-async-errors';
-
-import routes from './routes';
+import logger from './app/services/LogService';
 
 import HttpException from './config/HttpException';
 
+import routes from './routes';
+
 class App {
   express: express.Application;
-  logger!: Logger;
 
   constructor() {
     this.express = express();
-    this.winston();
     this.middlewares();
     this.routes();
     this.exceptionHandler();
@@ -37,32 +33,13 @@ class App {
     this.express.use(routes);
   }
 
-  private winston() {
-    const {
-      combine, timestamp, prettyPrint,
-    } = format;
-
-    this.logger = createLogger({
-      format: combine(
-        timestamp(),
-        prettyPrint(),
-      ),
-      transports: [
-        new transports.Console(),
-        new transports.File({ filename: './src/logs/error.log', level: 'error' }),
-        new transports.File({ filename: './src/logs/info.log', level: 'info' }),
-      ],
-      exitOnError: false,
-    });
-  }
-
   private exceptionHandler() {
     // Middlewate with four params it is a middleware for handle exceptions.
     this.express.use(async (err: HttpException, req: Request, res: Response, next: NextFunction) => {
       if (process.env.NODE_ENV === 'development') {
         const errors = await new Youch(err, req).toJSON();
 
-        this.logger.error(`${ err.status || 500 } - ${ err.name } - ${ err.message } - ${ req.originalUrl } - ${ req.method } - ${ req.ip } - ${ err.stack }`);
+        logger.error(`${ err.status || 500 } - ${ err.name } - ${ err.message } - ${ req.originalUrl } - ${ req.method } - ${ req.ip } - ${ err.stack }`);
 
         return res.status(500).json(errors);
       }
---
+++ src/app/services/LogService.ts
@@ -0,0 +1,50 @@
+import {
+  createLogger, format, transports, Logger,
+} from 'winston';
+
+class LogService {
+  logger!: Logger;
+  baseOptions = {
+    handleExceptions: true,
+    maxsize: 5242880, // 5MB
+    maxFiles: 5,
+  }
+
+  constructor() {
+    this.winston();
+  }
+
+  private winston() {
+    const {
+      combine, timestamp, prettyPrint,
+    } = format;
+
+    this.logger = createLogger({
+      format: combine(
+        timestamp(),
+        prettyPrint(),
+      ),
+      transports: [
+        new transports.Console(),
+        new transports.File({
+          filename: './src/logs/error.log',
+          level: 'error',
+          ...this.baseOptions,
+        }),
+        new transports.File({
+          filename: './src/logs/info.log',
+          level: 'info',
+          ...this.baseOptions,
+        }),
+        new transports.File({
+          filename: './src/logs/debug.log',
+          level: 'debug',
+          ...this.baseOptions,
+        }),
+      ],
+      exitOnError: false,
+    });
+  }
+}
+
+export default new LogService().logger;
